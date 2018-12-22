4ef2af59c2dff722adbd6d6dd902c137cfd4d352
Aleksey Krichevskiy <boolivar@gmail.com>
2018-12-22 03:20:09 +0300

    scrips shebang

--- docker-build.sh
+++ docker-build.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 docker build --build-arg ANDROID_SDK_VERSION=$1 --build-arg ANDROID_BUILD_TOOLS=$2 -t ${DOCKER_USERNAME}/androidsdk:$3 .
--- docker-push.sh
+++ docker-push.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 docker login -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD} && docker push ${DOCKER_USERNAME}/androidsdk:$1
